da625cae32ae668e00a65b0378e5bc9e65b43e5f
Stef Ruinard <stefruinard@hotmail.com>
2021-08-24 07:49:55 +0200

    refactor

--- modelblessing/__init__.py
+++ modelblessing/__init__.py
@@ -1,16 +1,17 @@
 import azure.functions as func
 import azure.durable_functions as df
-import config
 from datetime import timedelta
+import os
+
 
 
 def orchestrator_function(context: df.DurableOrchestrationContext):
     headers = {"Content-Type": "application/json"}
     body = {
         "instance_id": context._instance_id,
-        "event_url": config.get_event_url()
+        "event_url": get_event_url()
     }
-    _ = yield context.call_http('POST', config.get_workflow_url(), content=body, headers=headers)
+    _ = yield context.call_http('POST', get_workflow_url(), content=body, headers=headers)
 
     expiration = context.current_utc_datetime + timedelta(hours=24)
     timeout_task = context.create_timer(expiration)
@@ -21,5 +22,12 @@ def orchestrator_function(context: df.DurableOrchestrationContext):
     second_response = yield context.call_activity('activity-after-longrunning-task')
     return second_response
 
+def get_event_url():
+    event_url = os.environ.get("EVENT_URL", "")
+    return event_url
+
+def get_workflow_url():
+    workflow_url = os.environ.get("WORKFLOW_URL", "")
+    return workflow_url
 
 main = df.Orchestrator.create(orchestrator_function)
